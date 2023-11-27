cbbfe1bc95c9dd251dfa91fbab779a7edeae066d
Joshua Krause <52180639+JoKra1@users.noreply.github.com>
2023-11-27 13:39:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if "CONDA_PREFIX" in os.environ:
         eigen_path = os.environ["CONDA_PREFIX"] + "/include/eigen3"
 
 if eigen_path is None:
-    eigen_path = "usr" + "/include/eigen3"
+    eigen_path = "usr/local/" + "/include/eigen3"
 
 # Create Pybind setuptools extension
 ext = Pybind11Extension(name='cpp_solvers',
