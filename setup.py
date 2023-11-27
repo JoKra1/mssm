cbedd09b063dfd361c6b8cff3ef8d1deeac1ddaf
Joshua Krause <52180639+JoKra1@users.noreply.github.com>
2023-11-27 12:16:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,18 @@ from pybind11.setup_helpers import Pybind11Extension
 import os
 
 # Eigen can either be provided by conda or needs to be in "usr/include/eigen3"
-# ToDo: The second might break under windows.
+# ToDo: The latter might break under windows.
 print(os.environ["CONDA_PREFIX"])
 eigen_path = None
 if "CONDA_PREFIX" in os.environ:
     if os.path.isdir(os.environ["CONDA_PREFIX"] + "/include/eigen3"):
         eigen_path = os.environ["CONDA_PREFIX"] + "/include/eigen3"
+
+# Get github env
+ev = os.getenv('GITHUB_ENV')
+print(ev)
+if not ev is None:
+    eigen_path = ev.CONDA_PREFIX + "/include/eigen3"
 
 if eigen_path is None:
     eigen_path = "usr" + "/include/eigen3"
