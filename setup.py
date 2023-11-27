6676b60634c62afd68947fdd371d3a8d00cc3f2e
Joshua Krause <52180639+JoKra1@users.noreply.github.com>
2023-11-27 16:17:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,11 @@ import os
 # Eigen can either be provided by conda or needs to be in "usr/include/eigen3"
 # ToDo: The latter might break under windows.
 
-SETUP_DIRECTORY = Path(__file__).resolve().parent
-
 class get_eigen_include(object):
-    # Copied from: https://github.com/MatPiq/RlassoModels/blob/master/setup.py
-    # I hope this might help me with my building issues, because it was suggested in this
-    # thread, that it might work: https://github.com/readthedocs/readthedocs.org/issues/9034
-    EIGEN3_URL = "https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip"
-    EIGEN3_DIRNAME = "eigen-3.4.0"
+    # Based on the implementation from: https://github.com/MatPiq/RlassoModels/blob/master/setup.py
+    # This finally, allowed me to fix my building issues since the author of the repo above had
+    # the same problem: https://github.com/readthedocs/readthedocs.org/issues/9034.
+    # I made some changes to stream-line the cloning into Eigen a bit.
 
     def __str__(self) -> str:
         eigen_include_dir = os.environ.get("EIGEN3_INCLUDE_DIR", None)
@@ -22,44 +19,23 @@ class get_eigen_include(object):
         if eigen_include_dir is not None:
             return eigen_include_dir
 
-        target_dir = SETUP_DIRECTORY / self.EIGEN3_DIRNAME
+        # Local build based on conda
+        if "CONDA_PREFIX" in os.environ:
+            if os.path.isdir(os.environ["CONDA_PREFIX"] + "/include/eigen3"):
+                return os.environ["CONDA_PREFIX"] + "/include/eigen3"
+
+        # Local build with alternative accepted location.
+        if os.path.isdir("usr/local/include/eigen3"):
+            return "usr/local/include/eigen3"
+
+        target_dir = Path(__file__).resolve().parent / "eigen"
         if target_dir.exists():
             return target_dir.name
 
-        #download_target_dir = SETUP_DIRECTORY / "eigen3.zip"
-        #import zipfile
-
-        #import requests
-
-        #response = requests.get(self.EIGEN3_URL, stream=True)
-        #with download_target_dir.open("wb") as ofs:
-        #    for chunk in response.iter_content(chunk_size=1024):
-        #        ofs.write(chunk)
-
-        #with zipfile.ZipFile(download_target_dir) as ifs:
-        #    ifs.extractall()
-
+        # See: https://gist.github.com/plembo/a786ce2851cec61ac3a051fcaf3ccdab
         git.Repo.clone_from("https://gitlab.com/libeigen/eigen.git",target_dir,branch="3.4.0")
 
         return target_dir.name
-
-eigen_path = None
-if "CONDA_PREFIX" in os.environ:
-    if os.path.isdir(os.environ["CONDA_PREFIX"] + "/include/eigen3"):
-        eigen_path = os.environ["CONDA_PREFIX"] + "/include/eigen3"
-
-if os.getenv('CI') is not None:
-    print("CI")
-    c_path = os.getcwd()
-    if "eigen" not in os.listdir(c_path):
-        print("Cloning into Eigen.")
-        git.Repo.clone_from("https://gitlab.com/libeigen/eigen.git",c_path+"/eigen",branch="3.4.0")
-    print(os.listdir(c_path))
-    eigen_path = c_path + "/eigen/Eigen"
-    print(eigen_path)
-    
-if eigen_path is None:
-    eigen_path = "usr/local/" + "/include/eigen3"
 
 # Create Pybind setuptools extension
 ext = Pybind11Extension(name='cpp_solvers',
