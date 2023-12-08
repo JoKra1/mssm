Smoothing penalty delta calculation bug fix

src/mssm/models.py
@@ -768,7 +768,11 @@ class sMsGAMM(MSSM):
         # Now we can decode.
         with mp.Pool(processes=self.cpus) as pool:
             _,states_max,_ = self.__decode_all_states(pool,cov,self.__pi,self.__TR,s_log_o_probs,dur_log_probs,var_map)
-            max_states_flat = np.array([st for s in states_max for st in s],dtype=int)
+
+            if not self.mvar_by is None:
+                max_states_flat = np.array([st for s in states_max for _ in range(len(factor_levels[self.mvar_by])) for st in s],dtype=int)
+            else:
+                max_states_flat = np.array([st for s in states_max for st in s],dtype=int)
         
         return llk_hist,max_states_flat
             