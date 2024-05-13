e9895813facdb094015151e40f65d1f0d132b8e3
Joshua Krause <52180639+JoKra1@users.noreply.github.com>
2024-05-13 17:27:42 +0200

    Tutorial: iterative cross-product + test case for iterative cross-product

--- tests/test_gamm.py
+++ tests/test_gamm.py
@@ -154,3 +154,40 @@ class Test_GAMM:
         diff = np.abs(np.round([p.lam for p in self.model.formula.penalties],decimals=3) - np.array([0.004, 0.006, 5842.507, 1101786.56 , 328846.811, 174267.629, 162215.095, 1178.787, 0.119, 2.166]))
         rel_diff = diff/np.array([0.004, 0.006, 5842.507, 1101786.56 , 328846.811, 174267.629, 162215.095, 1178.787, 0.119, 2.166])
         assert np.max(rel_diff) < 1e-7
+
+
+class Test_BIG_GAMM:
+
+    file_paths = [os.path.join(os.path.dirname(os.path.dirname(__file__)), f'tutorials/data/GAMM/sim_dat_cond_{cond}.csv') for cond in ["a","b"]]
+
+    formula = Formula(lhs=lhs("y"), # The dependent variable - here y!
+                      terms=[i(), # The intercept, a
+                               l(["cond"]), # For cond='b'
+                               f(["time"],by="cond",constraint=ConstType.QR), # to-way interaction between time and cond; one smooth over time per cond level
+                               f(["x"],by="cond",constraint=ConstType.QR), # to-way interaction between x and cond; one smooth over x per cond level
+                               f(["time","x"],by="cond",constraint=ConstType.QR), # three-way interaction
+                               fs(["time"],rf="sub")], # Random non-linear effect of time - one smooth per level of factor sub
+                        data=None, # No data frame!
+                        file_paths=file_paths, # Just a list with paths to files.
+                        print_warn=False)
+        
+    model = GAMM(formula,Gaussian())
+
+    model.fit(extension_method_lam = "mult")
+
+    def test_GAMMedf(self):
+        assert round(self.model.edf,ndigits=3) == 153.601
+
+    def test_GAMMTermEdf(self):
+        diff = np.abs(np.round(self.model.term_edf,decimals=3) - np.array([6.892, 8.635, 1.181, 1.001, 1.001, 1.029, 131.861]))
+        rel_diff = diff/np.array([6.892, 8.635, 1.181, 1.001, 1.001, 1.029, 131.861])
+        assert np.max(rel_diff) < 1e-7
+    
+    def test_GAMMsigma(self):
+        _, sigma = self.model.get_pars()
+        assert round(sigma,ndigits=3) == 577.196
+    
+    def test_GAMMlam(self):
+        diff = np.abs(np.round([p.lam for p in self.model.formula.penalties],decimals=3) - np.array([0.004, 0.006, 5842.507, 1101786.56 , 328846.811, 174267.629, 162215.095, 1178.787, 0.119, 2.166]))
+        rel_diff = diff/np.array([0.004, 0.006, 5842.507, 1101786.56 , 328846.811, 174267.629, 162215.095, 1178.787, 0.119, 2.166])
+        assert np.max(rel_diff) < 1e-7
