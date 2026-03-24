36812db99dcf4fd05bb32587fc8a623f94862ada
Joshua Krause <52180639+JoKra1@users.noreply.github.com>
2026-03-24 12:32:31 +0100

    Better divergence checks implemented for NUTS sampler

--- src/mssm/src/cpp/mcmc.cpp
+++ src/mssm/src/cpp/mcmc.cpp
@@ -16,14 +16,13 @@ namespace py = pybind11;
 
 double compute_energy(
     const Eigen::Ref<Eigen::MatrixXd> &r,
-    const Eigen::SparseMatrix<double,0,long long int> &M
+    const Eigen::SparseMatrix<double,0,long long int> &Minv
 )
 /*
-Computes current kinetic energy of Hamiltonian after subtracting logp(state) as defined
-by Betancourt (2013,2018).
+Computes current kinetic energy of Hamiltonian as defined by Betancourt (2013,2018).
 */
 {
-    Eigen::MatrixXd energy = 0.5 * r.transpose() * M * r;
+    Eigen::MatrixXd energy = 0.5 * r.transpose() * Minv * r;
     return energy(0,0);
 }
 
@@ -37,14 +36,14 @@ leap_frog(
     const Eigen::Ref<Eigen::MatrixXd> &state,
     const Eigen::Ref<Eigen::MatrixXd> &grad,
     const Eigen::Ref<Eigen::MatrixXd> &r,
-    const Eigen::SparseMatrix<double,0,long long int> &M,
+    const Eigen::SparseMatrix<double,0,long long int> &Minv,
     double epsilon,
     const std::function<double(const Eigen::Ref<Eigen::MatrixXd>&)> &llk_fun,
     const std::function<Eigen::MatrixXd(const Eigen::Ref<Eigen::MatrixXd>&)> &grad_fun
 )
 /*
 Leapfrog algorithm as described in Algorithm 1 of Hoffman & Gelman (2014). Adapted to work
-with Riemannian metric ``M``, as described in their discussion.
+with inverse of metric ``Minv``, as described in their discussion.
 */
 {
     
@@ -55,7 +54,7 @@ with Riemannian metric ``M``, as described in their discussion.
     
     // First update to rprime and update to stateprime
     rprime = r + 0.5 * epsilon * grad;
-    stateprime = state + epsilon * M * rprime;
+    stateprime = state + epsilon * Minv * rprime;
 
     // Compute new grad and llk
     double llkprime = llk_fun(stateprime);
@@ -67,13 +66,60 @@ with Riemannian metric ``M``, as described in their discussion.
     return std::make_tuple(std::move(stateprime),std::move(gradprime),std::move(rprime),llkprime);
 }
 
+int check_dynamic_divergence(
+    const Eigen::Ref<Eigen::MatrixXd> &rb1,
+    const Eigen::Ref<Eigen::MatrixXd> &rb2,
+    const Eigen::Ref<Eigen::MatrixXd> &re1,
+    const Eigen::Ref<Eigen::MatrixXd> &re2,
+    const Eigen::Ref<Eigen::MatrixXd> &rsum1,
+    const Eigen::Ref<Eigen::MatrixXd> &rsum2,
+    const Eigen::SparseMatrix<double,0,long long int> &Minv
+)
+/*
+Computes the dynamic termination criterion proposed by M. J. Betancourt (2013,2018)
+*/
+{
+    int s = 1; // Divergence criterion 
+    Eigen::MatrixXd rsum = rsum1 + rsum2;
+    Eigen::MatrixXd rsum_sub = rsum1 + rb2; // first rsum1 + rb2 then rsum2 + re1
+
+    Eigen::MatrixXd rbsharp = Minv * rb1;
+    Eigen::MatrixXd r2bsharp = Minv * rb2;
+    Eigen::MatrixXd r1esharp = Minv * re1;
+    Eigen::MatrixXd resharp = Minv * re2;
+
+    // Now integrate over r (Betancourt, 2013,2018)...
+
+    // ... first across sub-trees ...
+    Eigen::MatrixXd Db = rbsharp.transpose() * rsum;
+    Eigen::MatrixXd De = resharp.transpose() * rsum;
+
+    s *= (int(Db(0,0) > 0) * int(De(0,0) > 0));
+
+    // ... then between sub-trees once ...
+    Db = rbsharp.transpose() * rsum_sub;
+    De = r2bsharp.transpose() * rsum_sub;
+
+    s *= (int(Db(0,0) > 0) * int(De(0,0) > 0));
+
+    // ... and twice.
+    rsum_sub = rsum2 + re1;
+
+    Db = r1esharp.transpose() * rsum_sub;
+    De = resharp.transpose() * rsum_sub;
+
+    s *= (int(Db(0,0) > 0) * int(De(0,0) > 0));
+
+    // Now return
+    return s;
+}
+
 
 std::tuple<Eigen::MatrixXd, // statem
            Eigen::MatrixXd, // statep
            Eigen::MatrixXd, // stateprime
            Eigen::MatrixXd, // rm
            Eigen::MatrixXd, // rp
-           Eigen::MatrixXd, // rprime
            Eigen::MatrixXd, // rsum
            size_t, // nprime
            int, // sprime
@@ -84,7 +130,7 @@ std::tuple<Eigen::MatrixXd, // statem
 build_tree(
     const Eigen::Ref<Eigen::MatrixXd> &state,
     const Eigen::Ref<Eigen::MatrixXd> &r,
-    const Eigen::SparseMatrix<double,0,long long int> &M,
+    const Eigen::SparseMatrix<double,0,long long int> &Minv,
     double logu,
     int v,
     size_t j,
@@ -106,7 +152,6 @@ M. J. Betancourt (2013,2018) - requiring book-keeping for the additional rsum va
     Eigen::MatrixXd stateprime;
     Eigen::MatrixXd rm;
     Eigen::MatrixXd rp;
-    Eigen::MatrixXd rprime;
     Eigen::MatrixXd rsum;
     size_t nprime;
     int sprime;
@@ -119,12 +164,13 @@ M. J. Betancourt (2013,2018) - requiring book-keeping for the additional rsum va
     {
         Eigen::MatrixXd grad = grad_fun(state);
         Eigen::MatrixXd gradprime;
+        Eigen::MatrixXd rprime;
         
-        std::tie(stateprime,gradprime,rprime,Lprime) = leap_frog(state,grad,r,M,
+        std::tie(stateprime,gradprime,rprime,Lprime) = leap_frog(state,grad,r,Minv,
                                                                  v*epsilon,llk_fun,
                                                                  grad_fun);
     
-        double Hprime = Lprime - compute_energy(rprime,M);
+        double Hprime = Lprime - compute_energy(rprime,Minv);
 
         // log of prob ratios
         double lp = Hprime - H0;
@@ -144,62 +190,77 @@ M. J. Betancourt (2013,2018) - requiring book-keeping for the additional rsum va
         //py::print(logu,Hprime,sprime);
 
         return std::make_tuple(std::move(statem),std::move(statep),std::move(stateprime),
-                               std::move(rm),std::move(rp),std::move(rprime),std::move(rsum),
+                               std::move(rm),std::move(rp),std::move(rsum),
                                nprime,sprime,aprime,naprime,Lprime);
     }
     else
     {
         // Recursion - build left and right sub-trees implicitly
-
         std::tie(statem,statep,stateprime,rm,rp,
-                 rprime,rsum,nprime,sprime,
-                 aprime,naprime,Lprime) = build_tree(state,r,M,logu,v,j-1,epsilon,H0,
+                 rsum,nprime,sprime,
+                 aprime,naprime,Lprime) = build_tree(state,r,Minv,logu,v,j-1,epsilon,H0,
                                                      DeltaMax,llk_fun,grad_fun);
-        
+            
         if (sprime == 1)
         {
             
-            // Double primes and throwaways for p and m
-            Eigen::MatrixXd statemp;
-            Eigen::MatrixXd statepm;
-            Eigen::MatrixXd stateprime2;
-            Eigen::MatrixXd rmp;
-            Eigen::MatrixXd rpm;
-            Eigen::MatrixXd rprime2;
-            Eigen::MatrixXd rsum2;
+            // Double primes and extra variables to compute points in terms of end/beginning
+            // for easier implementation of divergence check by Betancourt (2013,2018)
+            Eigen::MatrixXd statep2; // + step for backward integration (at beginning of tree 2)
+            Eigen::MatrixXd statem2; // - step for forward integration (at beginning of tree 2)
+            Eigen::MatrixXd stateprime2; // proposal of tree 2
+            Eigen::MatrixXd rb1; // Momentum at begin of tree 1
+            Eigen::MatrixXd re1; // Momentum at end of tree 1
+            Eigen::MatrixXd rb2; // Momentum at begin of tree 2
+            Eigen::MatrixXd re2; // Momentum at end of tree 2
+            Eigen::MatrixXd rsum2; // Integration over tree 2
+            Eigen::MatrixXd rsum_sub; // first rsum + rb2 then rsum2 + re1
+
+            // Rest of prime variables
             size_t nprime2;
             int sprime2;
             double aprime2;
             size_t naprime2;
-            double Lprime2;
+            double Lprime2;        
             
             if (v == -1)
-            {
-                // Backward integration
-                std::tie(statem,statepm,stateprime2,rm,rpm,
-                         rprime2,rsum2,nprime2,sprime2,
-                         aprime2,naprime2,Lprime2) = build_tree(statem,rm,M,logu,v,j-1,epsilon,H0,
+            {   
+
+                // Backward integration - second sub-tree is to the left
+                
+                rb1 = rp; // momentum at beginning of tree 1
+                re1 = rm; // momentum at end of tree 1
+                
+                // Now integrate
+                std::tie(statem,statep2,stateprime2,rm,rb2,
+                         rsum2,nprime2,sprime2,
+                         aprime2,naprime2,Lprime2) = build_tree(statem,rm,Minv,logu,v,j-1,epsilon,H0,
                                                                 DeltaMax,llk_fun,grad_fun);
+                
+                re2 = rm; // momentum at end of tree 2
+
             }
             else
             {
-                // Forward integration
-                std::tie(statemp,statep,stateprime2,rmp,rp,
-                         rprime2,rsum2,nprime2,sprime2,
-                         aprime2,naprime2,Lprime2) = build_tree(statep,rp,M,logu,v,j-1,epsilon,H0,
+                // Forward integration - second sub-tree is to the right
+
+                rb1 = rm; // momentum at beginning of tree 1
+                re1 = rp; // momentum at end of tree 1
+                
+                // Now integrate
+                std::tie(statem2,statep,stateprime2,rb2,rp,
+                         rsum2,nprime2,sprime2,
+                         aprime2,naprime2,Lprime2) = build_tree(statep,rp,Minv,logu,v,j-1,epsilon,H0,
                                                                 DeltaMax,llk_fun,grad_fun);
+                                                                
+                re2 = rp; // momentum at end of tree 2
             }
-
-            // integrate over r (Betancourt, 2013,2018)
-
-            rsum += rsum2;
 
             std::random_device rd;
             std::mt19937 gen(rd());
             std::uniform_real_distribution<> U(0.0, 1.0);
             
             // Accept state'' with probability
-
             if (
                 (nprime + nprime2 > 0) &
                 (((1.0 * nprime2) / (nprime + nprime2)) > U(gen))
@@ -212,20 +273,20 @@ M. J. Betancourt (2013,2018) - requiring book-keeping for the additional rsum va
             aprime += aprime2;
             naprime += naprime2;
 
-            // Now new divergence check by Betancourt (2013,2018)
-            Eigen::MatrixXd rpsharp = M * rp;
-            Eigen::MatrixXd rmsharp = M * rm;
-            Eigen::MatrixXd Dp = rpsharp.transpose() * rsum;
-            Eigen::MatrixXd Dm = rmsharp.transpose() * rsum;
+            // integrate over r (Betancourt, 2013,2018) to check convergence
+            sprime = sprime2 * check_dynamic_divergence(rb1,rb2,re1,re2,rsum,rsum2,Minv);
+            
+            // Update overall rsum after check
+            rsum += rsum2;
 
-            sprime = sprime2 * (int(Dp(0,0) > 0) * int(Dm(0,0) > 0));
-
+            // Now update nprime and return
             nprime += nprime2;
 
         }
+
         // Return else case
         return std::make_tuple(std::move(statem),std::move(statep),std::move(stateprime),
-                               std::move(rm),std::move(rp),std::move(rprime),std::move(rsum),
+                               std::move(rm),std::move(rp),std::move(rsum),
                                nprime,sprime,aprime,naprime,Lprime);
     }
 }
@@ -250,8 +311,8 @@ Heuristic algorithm to determine an initial value for epsilon as described in Al
 Hoffman & Gelman (2014).
 */
 {
-    // Build M
-    Eigen::Map<Eigen::SparseMatrix<double,0,long long int>> M (  
+    // Build Minv
+    Eigen::Map<Eigen::SparseMatrix<double,0,long long int>> Minv (  
         Mrows,Mcols,Mnnz,
         (Eigen::SparseMatrix<double,0,long long int>::StorageIndex*) Midptr.data(),
         (Eigen::SparseMatrix<double,0,long long int>::StorageIndex*) Mindices.data(),
@@ -270,13 +331,13 @@ Hoffman & Gelman (2014).
     Eigen::MatrixXd rprime;
     double Lprime;
 
-    double H = L - compute_energy(r,M);
+    double H = L - compute_energy(r,Minv);
 
-    std::tie(stateprime,gradprime,rprime,Lprime) = leap_frog(state,grad,r,M,
+    std::tie(stateprime,gradprime,rprime,Lprime) = leap_frog(state,grad,r,Minv,
                                                              epsilon,llk_fun,
                                                              grad_fun);
     
-    double Hprime = Lprime - compute_energy(rprime,M);
+    double Hprime = Lprime - compute_energy(rprime,Minv);
 
     // log of prob ratios
     double lp = Hprime - H;
@@ -291,11 +352,11 @@ Hoffman & Gelman (2014).
         epsilon *= pow(2,alpha);
 
         // Update Hamiltonian and log of prob ratio
-        std::tie(stateprime,gradprime,rprime,Lprime) = leap_frog(state,grad,r,M,
+        std::tie(stateprime,gradprime,rprime,Lprime) = leap_frog(state,grad,r,Minv,
                                                                  epsilon,llk_fun,
                                                                  grad_fun);
         
-        Hprime = Lprime - compute_energy(rprime,M);
+        Hprime = Lprime - compute_energy(rprime,Minv);
 
         lp = Hprime - H;
     }
@@ -337,7 +398,8 @@ std::tuple<Eigen::VectorXd,
 /*
 Complete steps of algorithm 6 as defined by Hoffman & Gelman (2014) to sample next states and llks
 of a log-joint via No-U-Turn Sampler defined by Hoffman & Gelman (2014).
-Adapted to work with any Riemannian metric, based on M. J. Betancourt (2013,2018).
+Adapted to work with any Riemannian metric, based on M. J. Betancourt (2013,2018), as implemented
+in STAN (Carpenter et al., 2017) as well.
 
 References:
  - Hoffman, M. D., & Gelman, A. (2014). The No-U-Turn Sampler: Adaptively Setting Path Lengths in\
@@ -346,17 +408,20 @@ References:
     (No. arXiv:1304.1920). arXiv. https://doi.org/10.48550/arXiv.1304.1920
  - Betancourt, M. (2018). A Conceptual Introduction to Hamiltonian Monte Carlo\
     (No. arXiv:1701.02434). arXiv. https://doi.org/10.48550/arXiv.1701.02434
+ - Carpenter, B., Gelman, A., Hoffman, M. D., Lee, D., Goodrich, B., Betancourt, M., … Riddell, A.\
+    (2017). Stan: A Probabilistic Programming Language. Journal of Statistical Software,\
+    https://doi.org/10.18637/jss.v076.i01
 */
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> U(0.0, 1.0);
     std::exponential_distribution<> ed(1);
-    double DeltaMax = 10000;
+    double DeltaMax = 1000;
     long long int n_coef = Mrows;
 
-    // Construct M from buffers
-    Eigen::Map<Eigen::SparseMatrix<double,0,long long int>> M (  
+    // Construct Minv from buffers
+    Eigen::Map<Eigen::SparseMatrix<double,0,long long int>> Minv (  
         Mrows,Mcols,Mnnz,
         (Eigen::SparseMatrix<double,0,long long int>::StorageIndex*) Midptr.data(),
         (Eigen::SparseMatrix<double,0,long long int>::StorageIndex*) Mindices.data(),
@@ -377,7 +442,7 @@ References:
         Eigen::MatrixXd r0 = r_sampler();
 
         // Compute H0
-        double H0 = cL - compute_energy(r0,M);
+        double H0 = cL - compute_energy(r0,Minv);
 
         // Sample log(u) where u is U[0,exp(H0)]
         // If u1 is U[0,1]
@@ -392,8 +457,7 @@ References:
         Eigen::MatrixXd statep = cstate;
         Eigen::MatrixXd rm = r0;
         Eigen::MatrixXd rp = r0;
-        Eigen::MatrixXd rsum;
-        rsum.setZero(n_coef,1);
+        Eigen::MatrixXd rsum = r0; // Integration variable required by Betancourt (2013,2018)
         size_t n = 1;
         int s = 1;
         double a;
@@ -401,13 +465,14 @@ References:
         size_t j = 0;
 
         // Single prime variables and throwaways for p and m
-        Eigen::MatrixXd statemp;
-        Eigen::MatrixXd statepm;
+        Eigen::MatrixXd statep2; // + step for backward integration
+        Eigen::MatrixXd statem2; // - step for forward integration
         Eigen::MatrixXd stateprime;
-        Eigen::MatrixXd rmp;
-        Eigen::MatrixXd rpm;
-        Eigen::MatrixXd rprime;
-        Eigen::MatrixXd rsumprime;
+        Eigen::MatrixXd rb1; // Momentum at begin of old trajectory/tree
+        Eigen::MatrixXd re1; // Momentum at end of old trajectory/tree
+        Eigen::MatrixXd rb2; // Momentum at begin of new trajectory/tree
+        Eigen::MatrixXd re2; // Momentum at end of new trajectory/tree
+        Eigen::MatrixXd rsumprime; // Integration over tree 2
         size_t nprime;
         int sprime;
         double Lprime;
@@ -419,22 +484,32 @@ References:
             if (v == -1)
             {
                 // Backward integration
-                std::tie(statem,statepm,stateprime,rm,rpm,
-                        rprime,rsumprime,nprime,sprime,
-                        a,na,Lprime) = build_tree(statem,rm,M,logu,v,j,epsilon,H0,
+
+                rb1 = rp; // momentum at beginning of old tree
+                re1 = rm; // momentum at end of old tree
+
+                std::tie(statem,statep2,stateprime,rm,rb2,
+                        rsumprime,nprime,sprime,
+                        a,na,Lprime) = build_tree(statem,rm,Minv,logu,v,j,epsilon,H0,
                                                 DeltaMax,llk,grad);
+                
+                re2 = rm; // momentum at end of new tree
             }
             else
             {
                 // Forward integration
-                std::tie(statemp,statep,stateprime,rmp,rp,
-                        rprime,rsumprime,nprime,sprime,
-                        a,na,Lprime) = build_tree(statep,rp,M,logu,v,j,epsilon,H0,
+
+                rb1 = rm; // momentum at beginning of old tree
+                re1 = rp; // momentum at end of old tree
+
+                std::tie(statem2,statep,stateprime,rb2,rp,
+                        rsumprime,nprime,sprime,
+                        a,na,Lprime) = build_tree(statep,rp,Minv,logu,v,j,epsilon,H0,
                                                 DeltaMax,llk,grad);
+                
+                re2 = rp; // momentum at end of new tree
             }
 
-            // integrate over r (Betancourt, 2013,2018)
-            rsum += rsumprime;
             //py::print(j,s,sprime,logu,v,nprime,n);
 
             if (sprime == 1)
@@ -450,13 +525,14 @@ References:
             // Update variables
             n += nprime;
 
-            // New divergence check by Betancourt (2013,2018)
-            Eigen::MatrixXd rpsharp = M * rp;
-            Eigen::MatrixXd rmsharp = M * rm;
-            Eigen::MatrixXd Dp = rpsharp.transpose() * rsum;
-            Eigen::MatrixXd Dm = rmsharp.transpose() * rsum;
+            // Integrate over r across and between trees for dynamic divergence check
+            // as proposed by (Betancourt, 2013,2018)
+            s = sprime * check_dynamic_divergence(rb1,rb2,re1,re2,rsum,rsumprime,Minv);
+            
+            // Update overall rsum after check
+            rsum += rsumprime;
 
-            s = sprime * (int(Dp(0,0) > 0) * int(Dm(0,0) > 0));
+            // Next doubling of tree
             j += 1;
         }
 
